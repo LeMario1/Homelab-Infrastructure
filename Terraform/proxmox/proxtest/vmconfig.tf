75a3994ea24becd98676f65837b2d3ed626d29ec
Mario <mariomedina6464@gmail.com>
2026-04-04 05:02:33 -0700

    Defined CPU block and specified 2 cores. Added description for proxmox notes section. Defined network block and specified,virtio, vmbr1 (data interface on pve0,) and dhcp - enabling internet access. Enabled qemu-guest-agent.

--- Terraform/proxmox/proxtest/vmconfig.tf
+++ Terraform/proxmox/proxtest/vmconfig.tf
@@ -2,14 +2,22 @@ resource "proxmox_vm_qemu" "terraform_boot_test" {
   name        = "terraform-boot-test"
   target_node = "pve0"
   description = "A test vm managed by terraform."
+  agent       = "1"
+  ipconfig0   = "ip=dhcp"
 
   clone = "cloud-init-test"
 
   # Define cpu options
   cpu {
-    cores = 2
+    cores = "2"
   }
 
+  # Define network device
+  network {
+    id     = "0"
+    model  = "virtio"
+    bridge = "vmbr1" #vmbr1 is data interface on pve0
+  }
 
   # disk must be specified here because of a bug with cloning. Cloning should work, but terraform leaves the disk unnatached, making the machine unbootable.
   scsihw = "virtio-scsi-single"
@@ -18,12 +26,12 @@ resource "proxmox_vm_qemu" "terraform_boot_test" {
       scsi0 {
         disk {
           storage    = "zfs-nvme"
-          size       = 16
-          emulatessd = true
-          iothread   = false
-          discard    = true
-          backup     = true
-          replicate  = true
+          size       = "16"
+          emulatessd = "true"
+          iothread   = "false"
+          discard    = "true"
+          backup     = "true"
+          replicate  = "true"
         }
       }
     }
