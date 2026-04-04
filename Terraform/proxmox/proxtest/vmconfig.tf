c309ec783c4da76ad9f0be84c4bb79d6dc9818b7
Mario <mariomedina6464@gmail.com>
2026-04-04 04:30:49 -0700

    Defines a cpu block and specifies 2 cores for the vm. Also adds a description for the notes field in proxmox gui.

--- Terraform/proxmox/proxtest/vmconfig.tf
+++ Terraform/proxmox/proxtest/vmconfig.tf
@@ -1,11 +1,18 @@
 resource "proxmox_vm_qemu" "terraform_boot_test" {
   name        = "terraform-boot-test"
   target_node = "pve0"
+  description = "A test vm managed by terraform."
 
   clone = "cloud-init-test"
 
-  scsihw = "virtio-scsi-single"
+  # Define cpu options
+  cpu {
+    cores = 2
+  }
 
+
+  # disk must be specified here because of a bug with cloning. Cloning should work, but terraform leaves the disk unnatached, making the machine unbootable.
+  scsihw = "virtio-scsi-single"
   disks {
     scsi {
       scsi0 {
